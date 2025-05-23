8d78e3cb61393dd47e5251b705951771c1207873
dedevOP <akoca113b@gmail.com>
2025-05-23 14:48:13 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "2rem", color: "#eab308" }}>MemeVault</h1>
+      <p style={{ marginTop: "1rem" }}>Your site is working! Let's build this thing 🔥</p>
+    </div>
+  );
+}
